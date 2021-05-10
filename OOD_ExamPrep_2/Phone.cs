5b043ec4139b040d480a631f5c529e2aac4f0881
Andrew Casey <60218234+AndrewC20@users.noreply.github.com>
2021-05-10 14:17:10 +0100

    part 2 and 3 complete

--- OOD_ExamPrep_2/Phone.cs
+++ OOD_ExamPrep_2/Phone.cs
@@ -8,5 +8,27 @@ namespace OOD_ExamPrep_2
 {
     public class Phone
     {
+
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public string OperatingSystem { get; set; }
+        public string OS_Image { get; set; }
+        public string Phone_Image { get; set; }
+
+        public Phone()
+        {
+
+        }
+
+        public Phone (string name, decimal price, string operatingSystem, string os_image ="", string phone_image = "")
+        {
+
+        }
+
+        public void IncreasePrice(double increase)
+        {
+            Price *= (decimal)(increase + 1);
+        }
     }
 }
